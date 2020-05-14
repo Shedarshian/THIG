70026b062cadca8dcb969398280cbcb78564300d
Shedarshian <qitianyu1998@163.com>
2020-05-14 19:54:35 +0800

    first step

--- src/game.js
+++ src/game.js
@@ -1,14 +1,29 @@
+"use strict";
+
 var reimu = {
-    money: 0,
-    reputation: 0
-}
+    money: new Decimal(0),
+    reputation: new Decimal(0),
+    haveMoneyClicked: false
+};
 
 function giveMoneyByHand() {
-    reimu.money = reimu.money + 1;
-    updateMoney();
+    reimu.money = reimu.money.add(1);
+    reimu.haveMoneyClicked = true;
 }
 
 function updateMoney() {
     var element = document.getElementById("moneyAmount");
-    element.textContent = reimu.money.toString();
+    element.textContent = reimu.money.floor().toString();
+    var element = document.getElementById("reputationAmount");
+    element.textContent = reimu.reputation.floor().toString();
 }
+
+function gameLoop(diff) {
+    diff = diff / 1000;
+    if (reimu.haveMoneyClicked) {
+        reimu.money = reimu.money.add(Decimal.pow(1.05, reimu.reputation).times(diff));
+    }
+    updateMoney();
+}
+
+setInterval(function() { gameLoop(50); }, 50)
