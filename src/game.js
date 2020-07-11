71ef627185e55530b611026787cc4d175f65197a
9cai <sktjfzt@yahoo.com>
2020-07-11 21:58:41 +0800

    新按钮和函数的初步尝试，现在可以升级投币的数量了。

--- src/game.js
+++ src/game.js
@@ -1,14 +1,23 @@
 "use strict";
 
 var reimu = {
-    money: new Decimal(0),
+    money: new Decimal(10),
     reputation: new Decimal(0),
+    moneyBonus:new Decimal(0),
     haveMoneyClicked: false
 };
 
 function giveMoneyByHand() {
     reimu.money = reimu.money.add(1);
+    reimu.money = reimu.money.add(reimu.moneyBonus);
     reimu.haveMoneyClicked = true;
+}
+
+function upgradeMoneyGain() {
+    if (reimu.money.cmp(10) == 1) {
+    reimu.money = reimu.money.sub(10);
+    reimu.moneyBonus = reimu.moneyBonus.add(1);
+    }
 }
 
 function updateMoney() {
